80eaf941e3b88491a1ee243e212c495a1223840e
Yauheni Neshyn <Neshyn.Z@gmail.com>
2023-01-22 19:25:57 +0100

    Add test py script (draft)

---
+++ test.py
@@ -0,0 +1,16 @@
+x = 'From marquard@uct.ac.za'
+print(x[14:17])
+
+print(len('banana')*7)
+
+print(len('banana'))
+
+
+x = 'some text'
+
+print(str(x.upper()))
+print(x)
+
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+pos = data.find('.')
+print(data[pos:pos+3])
